d48740ff22f238a6456fdbefd082da079b03a225
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-10 20:27:34 +0530

    Problem #6

---
+++ contest/May_LeetCoding_Challenge/Week1/Majority_Element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int N = nums.size();
+        int maj_ele_index, count = 0;
+        for(int i=0; i<N; i++) {
+            if(count==0) {
+                maj_ele_index = i;
+                count = 1;
+            }
+            else {
+                if(nums[i]==nums[maj_ele_index])
+                    count++;
+                else
+                    count--;
+            }
+        }
+        return nums[maj_ele_index];
+    }
+};
