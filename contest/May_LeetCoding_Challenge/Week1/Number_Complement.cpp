94e6679c4046f00d3ed2f4d01bb5efddfaccaec4
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-10 21:00:23 +0530

    Problem #4

---
+++ contest/May_LeetCoding_Challenge/Week1/Number_Complement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int res=0, i=0;
+        while(num) {
+            res += (!(num%2)) << i++;
+            num >>= 1;
+        }
+        return i?res:1;
+    }
+};
