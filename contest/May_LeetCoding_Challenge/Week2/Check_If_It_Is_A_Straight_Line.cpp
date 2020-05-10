2bc0a22dffb5407016f4fb24be983d6aa39daa0c
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-10 20:22:34 +0530

    Problem #1

---
+++ contest/May_LeetCoding_Challenge/Week2/Check_If_It_Is_A_Straight_Line.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int N = coordinates.size();
+        sort(coordinates.begin(), coordinates.end());
+        if(N==1)
+            return true;
+        int slope = INT_MIN;
+        if((coordinates[1][0] - coordinates[0][0])!=0)
+            slope = (coordinates[1][1] - coordinates[0][1])/(coordinates[1][0] - coordinates[0][0]); 
+        for(int i=2; i<N; i++) {
+            int tmp_slope = INT_MIN;
+            if((coordinates[i][0] - coordinates[i-1][0]) != 0) 
+                tmp_slope = (coordinates[i][1] - coordinates[i-1][1]) / (coordinates[i][0] - coordinates[i-1][0]);
+            
+            if(tmp_slope != slope)
+                return false;
+        }
+        return true;
+    }
+};
