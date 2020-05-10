8448c76985b8279b230b8b8c7e7f367e29bd5408
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-10 20:46:10 +0530

    Problem #3

---
+++ contest/May_LeetCoding_Challenge/Week1/Ransome_Note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int len1 = ransomNote.length(), len2 = magazine.length();
+        vector<int> hash1(26, 0), hash2(26, 0);
+        for(int i=0; i<len1; i++)
+            hash1[(int)(ransomNote[i]-'a')]++;
+        for(int i=0; i<len2; i++)
+            hash2[(int)(magazine[i]-'a')]++;
+        for(int i=0; i<26; i++) {
+            if(hash1[i] > hash2[i])
+                return false;
+        }
+        return true;
+    }
+};
