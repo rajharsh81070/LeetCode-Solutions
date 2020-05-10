0a83409cdda0a7ad68b275ed1c6684295234bda6
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-10 20:25:20 +0530

    Problem #2

---
+++ contest/May_LeetCoding_Challenge/Week1/Jewels_And_Stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        unordered_map<char, int> mp;
+        int N = S.length();
+        for(int i=0; i<N; i++)
+            mp[S[i]]++;
+        int M = J.length();
+        int Ans = 0;
+        for(int i=0; i<M; i++)
+            Ans += mp[J[i]];
+        return Ans;
+    }
+};
