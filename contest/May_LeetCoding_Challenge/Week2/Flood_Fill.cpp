41d0deb50f31e6420df26da940030eaaf2537844
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-11 23:18:13 +0530

    Problem #4

---
+++ contest/May_LeetCoding_Challenge/Week2/Flood_Fill.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+  void helperFunction(vector<vector<int>> &image, int N, int M, int i, int j, int newColor, int oldColor)
+  {
+    if (i < 0 || i >= N || j < 0 || j >= M || image[i][j] != oldColor)
+      return;
+    image[i][j] = newColor;
+    helperFunction(image, N, M, i + 1, j, newColor, oldColor);
+    helperFunction(image, N, M, i, j + 1, newColor, oldColor);
+    helperFunction(image, N, M, i - 1, j, newColor, oldColor);
+    helperFunction(image, N, M, i, j - 1, newColor, oldColor);
+  }
+  vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int newColor)
+  {
+    int N = image.size();
+    int M = image[0].size();
+    // cout << N << " " << M;
+    int oldColor = image[sr][sc];
+    if (newColor == oldColor)
+      return image;
+    helperFunction(image, N, M, sr, sc, newColor, oldColor);
+    return image;
+  }
+};
