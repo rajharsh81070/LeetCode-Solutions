df9287b5d4a21cd8c6555b5a6fd2174b44a67fd2
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-14 21:30:34 +0530

    Problem #7

--- Problems/cpp/1_Two_Sum.cpp
+++ Problems/cpp/1_Two_Sum.cpp
@@ -2,18 +2,21 @@
 Question Link:- https://leetcode.com/problems/two-sum/
 */
 
-
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         int N = nums.size();
         unordered_map<int, int> mp;
-        for(int i=0; i<N; i++)
+        for (int i = 0; i < N; i++)
             mp[nums[i]] = i;
         int f_i, f_j;
-        for(int i=0; i<N; i++) {
-            if(mp.find(target-nums[i])!=mp.end() && mp[target-nums[i]]!=i) {
-                f_i = i, f_j = mp[target-nums[i]];
+        for (int i = 0; i < N; i++)
+        {
+            if (mp.find(target - nums[i]) != mp.end() && mp[target - nums[i]] != i)
+            {
+                f_i = i, f_j = mp[target - nums[i]];
                 break;
             }
         }
