079a08f89afc775695e9ee9db8afcbc74da8e236
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-14 21:38:20 +0530

    LeetCode Problem #402

---
+++ Problems/cpp/402_Remove_K_Digits.cpp
@@ -0,0 +1,32 @@
+/*
+Question Link :- https://leetcode.com/problems/remove-k-digits/
+*/
+
+class Solution
+{
+public:
+  string removeKdigits(string num, int k)
+  {
+    if (k >= num.size())
+      return string("0");
+
+    num += '0';
+    vector<char> vec;
+    for (int i = 0; i < num.size(); i++)
+    {
+      while (!vec.empty() && num[i] < vec.back() && k)
+      {
+        k--;
+        vec.pop_back();
+      }
+      vec.push_back(num[i]);
+    }
+    string ans;
+    for (int i = 0; i < vec.size() - 1; i++)
+    {
+      if (!ans.empty() || vec[i] != '0')
+        ans += vec[i];
+    }
+    return ans.empty() ? "0" : ans;
+  }
+};
